f735cdaed8d87cb52248414e92506a7b0924eac8
LeeSooJin <gowl0918@gmail.com>
2024-08-21 09:53:02 +0900

    OrderRepository,OrderService 작업

---
+++ src/main/java/com/appliances/recyle/service/OrderService.java
@@ -0,0 +1,13 @@
+package com.appliances.recyle.service;
+
+
+
+import com.appliances.recyle.dto.OrderDTO;
+
+import java.util.List;
+
+public interface OrderService {
+
+    List<OrderDTO> getOrders();
+
+}
--- src/test/java/com/appliances/recyle/service/ItemServiceTests.java
+++ src/test/java/com/appliances/recyle/service/ItemServiceTests.java
@@ -2,6 +2,7 @@ package com.appliances.recyle.service;
 
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
