9d930ea26ccd221e42f46deb6cb62d60fd342813
heeji-lee <dpgbanjdal@gmail.com>
2024-08-23 19:10:50 +0900

    아이템 등록 데이터베이스 연결 확인중

--- src/main/java/com/appliances/recyle/controller/AdminController.java
+++ src/main/java/com/appliances/recyle/controller/AdminController.java
@@ -1,11 +1,17 @@
 package com.appliances.recyle.controller;
 
+import com.appliances.recyle.dto.ItemDTO;
+import com.appliances.recyle.service.ItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log4j2
 @Controller
@@ -13,14 +19,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class AdminController {
 
+    private final ItemService itemService;
+
     @GetMapping("/product_register")
     public void registerGet() {
 
     }
 
     @PostMapping
-    public void registerPost() {
+    public void registerPost(@Valid ItemDTO itemDTO
+            , BindingResult bindingResult
+            , RedirectAttributes redirectAttributes
+            , Model model) {
+        Long ino = itemService.insert(itemDTO);
 
+        redirectAttributes.addFlashAttribute("result", bno);
+        redirectAttributes.addFlashAttribute("resultType", "register");
+        return "redirect:/board/list";
     }
 
     @GetMapping("/product")
--- src/test/java/com/appliances/recyle/repository/ItemRepositoryTests.java
+++ src/test/java/com/appliances/recyle/repository/ItemRepositoryTests.java
@@ -1,10 +1,16 @@
 package com.appliances.recyle.repository;
 
 
+import com.appliances.recyle.domain.Item;
+import com.appliances.recyle.domain.Member;
+import com.appliances.recyle.domain.MemberRole;
+import com.appliances.recyle.dto.ItemDTO;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Log4j2
@@ -13,8 +19,15 @@ public class ItemRepositoryTests {
     @Autowired
     ItemRepository itemRepository;
 
+    @Test
+    public void insertItem() {
+        Item item = Item.builder()
+                .iname("통돌이 세탁기")
+                .price(4500L)
+                .build();
 
-
+        itemRepository.save(item);
+    }
 
 }
 
