4690279c40a8b19cb60269f8f810a98231059a3e
LeeSooJin <gowl0918@gmail.com>
2024-08-26 09:25:31 +0900

    오류 주석처리 완료

--- src/main/java/com/appliances/recyle/controller/NoticeController.java
+++ src/main/java/com/appliances/recyle/controller/NoticeController.java
@@ -17,13 +17,13 @@ public class NoticeController {
     @Autowired
     private NoticeService noticeService;
 
-    @GetMapping("/echopickup/notice")
-    public String getNotices(Model model,
-                             @RequestParam(defaultValue = "0") int page,
-                             @RequestParam(defaultValue = "10") int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        Page<Notice> noticePage = noticeService.getNotices(pageable);
-        model.addAttribute("noticePage", noticePage);
-        return "echopickup/notice"; // 여기에서 경로를 지정합니다.
-    }
+//    @GetMapping("/echopickup/notice")
+//    public String getNotices(Model model,
+//                             @RequestParam(defaultValue = "0") int page,
+//                             @RequestParam(defaultValue = "10") int size) {
+//        Pageable pageable = PageRequest.of(page, size);
+//        Page<Notice> noticePage = noticeService.getNotices(pageable);
+//        model.addAttribute("noticePage", noticePage);
+//        return "echopickup/notice"; // 여기에서 경로를 지정합니다.
+//    }
 }
