0453b8319bcce6dc453fd6118a291ddbc56cc85f
LeeSooJin <gowl0918@gmail.com>
2024-09-13 15:30:47 +0900

    mypagecontroller 수정.

--- src/main/java/com/appliances/recyle/controller/MypageController.java
+++ src/main/java/com/appliances/recyle/controller/MypageController.java
@@ -1,6 +1,8 @@
 package com.appliances.recyle.controller;
 
+import com.appliances.recyle.domain.Member;
 import com.appliances.recyle.domain.Question;
+import com.appliances.recyle.service.MemberService2;
 import com.appliances.recyle.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -8,10 +10,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Log4j2
 @Controller
@@ -19,12 +26,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MypageController {
 
+    private final MemberService2 memberService2;
     private final QuestionService questionService;
 
     @GetMapping
     public String mypageGet() {
         return "/echopickup/mypage";
     }
+
+    // 주소 업데이트를 위한 POST 요청 처리
+    @PostMapping
+    public String updateAddressPost(@RequestBody Map<String, String> requestData) {
+        // 현재 로그인된 사용자 정보 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = ((User) authentication.getPrincipal()).getUsername();
+
+        // 사용자의 회원 정보를 가져오기
+        Member member = memberService2.getMemberByEmail(email)
+                .orElseThrow(() -> new RuntimeException("사용자 정보를 찾을 수 없습니다."));
+
+        // 새로운 정보 업데이트
+        String address = requestData.get("address");
+        String phone = requestData.get("phone");
+
+
+        // 새로운 정보 업데이트
+        member.setAddress(address);
+        member.setPhone(phone);
+        memberService2.save(member);
+
+        log.info("주소값 : "+address);
+        log.info("휴대폰: "+phone);
+        log.info(member);
+
+        if (address == null || phone == null) {
+            log.error("데이터가 비어 있습니다.");
+            return "redirect:/echopickup/mypage?error=true";
+        }
+        // 성공 시 마이페이지로 리다이렉트
+        return "redirect:/echopickup/mypage";
+    }
+
 
     @GetMapping("/changepassword")
     public void changepasswordGet() {
